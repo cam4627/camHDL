0db2c795eaae039082f30e803b54151f29c7b35a
Cameron Marsh <camcamarsh@gmail.com>
2026-09-21 10:22:48 -0400

    Complete segment display driver

--- lab3/lab3/bcd/src/seven_seg.vhd
+++ lab3/lab3/bcd/src/seven_seg.vhd
@@ -1,6 +1,6 @@
 -------------------------------------------------------------------------------
 -- Cameron Marsh
--- BCD Driver design for seven segment display
+-- BCD Driver design for seven segment display. Fully combinational, not clocked.
 -------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -16,6 +16,7 @@ entity seven_seg is
 end seven_seg;
 
 architecture beh of seven_seg is
+ -- Declarative section
 	constant SEG0 : std_logic_vector := "1000000";
 	constant SEG1 : std_logic_vector := "1111001";
 	constant SEG2 : std_logic_vector := "0100100";
@@ -27,21 +28,27 @@ architecture beh of seven_seg is
 	constant SEG8 : std_logic_vector := "0000000";
 	constant SEG9 : std_logic_vector := "0011000";
 	constant ERROR : std_logic_vector := "0000110";
+
 	begin
+-- Behavioral section
 	process(bcd)
 	begin
-		case bcd is
-			when "0000" => seven_seg_out <= SEG0;
-			when "0001" => seven_seg_out <= SEG1;
-			when "0010" => seven_seg_out <= SEG2;
-			when "0011" => seven_seg_out <= SEG3;
-			when "0100" => seven_seg_out <= SEG4;
-			when "0101" => seven_seg_out <= SEG5;
-			when "0110" => seven_seg_out <= SEG6;
-			when "0111" => seven_seg_out <= SEG7;
-			when "1000" => seven_seg_out <= SEG8;
-			when "1001" => seven_seg_out <= SEG9;
-			when others => seven_seg_out <= ERROR;
-		end case;
+		if reset = '1' then
+			seven_seg_out <= SEG0;
+		else
+			case bcd is
+				when "0000" => seven_seg_out <= SEG0;
+				when "0001" => seven_seg_out <= SEG1;
+				when "0010" => seven_seg_out <= SEG2;
+				when "0011" => seven_seg_out <= SEG3;
+				when "0100" => seven_seg_out <= SEG4;
+				when "0101" => seven_seg_out <= SEG5;
+				when "0110" => seven_seg_out <= SEG6;
+				when "0111" => seven_seg_out <= SEG7;
+				when "1000" => seven_seg_out <= SEG8;
+				when "1001" => seven_seg_out <= SEG9;
+				when others => seven_seg_out <= ERROR;
+			end case;
+		end if;
 	end process;
 end beh;
