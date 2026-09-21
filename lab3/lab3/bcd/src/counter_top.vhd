6370a409019dbb4e962bec87cc2e2f710f6b701e
Cameron Marsh <camcamarsh@gmail.com>
2026-09-21 10:27:49 -0400

    Add counter and adder components, create top-level template

---
+++ lab3/lab3/bcd/src/counter_top.vhd
@@ -0,0 +1,14 @@
+-------------------------------------------------------------------------------
+-- Cameron Marsh
+-- Top level for seven segment display counter
+-------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;      
+
+entity counter_top is
+
+end counter_top;  
+
+architecture beh of counter_top  is
+
+end beh;
